fa02cd746c8272b98c40450cf834d959be4d78c9
Douglas Hernandes <douglashernandes@icloud.com>
2025-12-01 09:11:40 -0300

    alterando componentes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,6 @@ function Header() {
         <>
             <HeaderPage>
                 <Title>Movie Database</Title>
-                <nav>
-                    <InputSearch type="text" id="srch-movie" placeholder="Search movie by title..." />
-                    <BtnSearch >Search</BtnSearch>
-                </nav>
             </HeaderPage>
         </>
     )
@@ -23,21 +19,4 @@ const Title = styled.h1`
   text-align: center;
 `;
 
-const InputSearch = styled.input`
-  padding: 10px;
-  width: 300px;
-  border-radius: 5px;
-  border: none;
-`;
-
-const BtnSearch = styled.button`
-  padding: 10px 15px;
-  border: none;
-  border-radius: 5px;
-  background: #e50914;
-  color: #fff;
-  font-weight: bold;
-  cursor: pointer;
-  margin-left: 5px;
-`;
 export default Header;
