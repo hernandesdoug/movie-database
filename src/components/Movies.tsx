06c295689d843d8bd18822622e55f5b985cb3c75
Douglas Hernandes <douglashernandes@icloud.com>
2025-12-01 18:07:14 -0300

    correcao interface e token

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,44 +1,47 @@
 import api from "../services/api.ts";
-import type { MovieProps } from "./Movie.ts";
+import type { MovieProps, Movie } from "./Movie.ts";
 import React, { useEffect, useState } from 'react';
 
 
-    let category = "popular";
-    let page = 1;
-    const Movies: React.FC = () => {
-    const [movies, setMovies] = useState<MovieProps[]>([]);
 
+const Movies: React.FC = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [category, setCategory] = useState("popular");
+    const [page, setPage] = useState(1);
+    const [isModalOpen, setisModalOpen] = useState(false);
 
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjE0OTEzMTE2YzQ1YmYxNmNkNTUwZjAzNDkwNzgzNCIsIm5iZiI6MTc1NjEyNDE0NC40MTgsInN1YiI6IjY4YWM1M2YwNjc0MjNhYWU1YzZjZWFmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vTUiCDlmoSNnGkHHvFo8INxf-DBltsmToBnNwJC2nuE";
+    const token = import.meta.env.VITE_TOKEN;
+
+    const fetchMovies = async () => {
+        try {
+            const response = await api.get<MovieProps>(`movie/${category}?language=en-US&page=${page}`,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+            if (response.status === 200) {
+                console.log(response.data)
+                setMovies(response.data.results);
+            } else {
+                console.log("Fail loading data", response.status);
+            }
+        } catch (error) {
+            console.error("Unexpected error!", error);
+        }
+    };
 
     useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await api.get<MovieProps[]>(`movie/${category}?language=en-US&page=${page}`,
-                    {
-                        headers: {
-                            'Authorization': `Bearer ${token}`
-                        }
-                    })
-                if (response.status === 200) {
-                    console.log(response.data)
-                    setMovies(response.data);
-                } else {
-                    console.log("Fail loading data", response.status);
-                }
-            } catch (error) {
-                console.error("Unexpected error!", error);
-            }
-        };
         fetchMovies();
-    }, [])
+    }, [page, category])
 
     return (
         <>
+        {isModalOpen && <h1>Modal</h1>}
             <ul>
                 {movies.map(movie => (
                     <li key={movie.id}>
-                        <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
                         <h1>${movie.title}</h1>
                         <h4>Rating: </h4><p>${movie.vote_average.toFixed(1)}</p>
                     </li>
