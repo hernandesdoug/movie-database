0069ea530a403729cf9be3d6fd73e30b34d05ad0
Douglas Hernandes <douglashernandes@icloud.com>
2025-12-09 17:53:38 -0300

    Adicionado modal do filme e ajuste nos botoes de categoria

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,13 +1,16 @@
 import api from "../services/api.ts";
 import type { MovieProps, Movie } from "./Movie.ts";
 import Pagination from "./Pagination.tsx";
-import type { CategoryProps } from "./Category.ts";
+import Category from "./Category.tsx";
 import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 
-const Movies: React.FC = ({category}) => {
+const Movies: React.FC = () => {
     const [movies, setMovies] = useState<Movie[]>([]);
     const [page, setPage] = useState(1);
+    const [category, setCategory] = useState("popular");
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+    const [showModal, setShowModal] = useState(false);
 
     const token = import.meta.env.VITE_TOKEN;
 
@@ -20,7 +23,6 @@ const Movies: React.FC = ({category}) => {
                     }
                 })
             if (response.status === 200) {
-                console.log(response.data)
                 setMovies(response.data.results);
                 setPage(pageNumber);
             } else {
@@ -40,11 +42,23 @@ const Movies: React.FC = ({category}) => {
       if (page > 1) fetchMovies(page - 1);
     };
 
+    const openModal = (movie: Movie) => {
+        setSelectedMovie(movie);
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedMovie(null);
+    };
+
     return (
         <>
+        <Category onChangeCategory={setCategory} />
         <GridMovies>
                 {movies.map(movie => (
-                    <MovieCard key={movie.id}>
+                    <MovieCard key={movie.id} 
+                    onClick={() => openModal(movie)}>
                         <PosterMovie src={`https://image.tmdb.org/t/p/w500${movie.poster_path}` }/>
                         <MovieTitle>${movie.title}</MovieTitle>
                         <Rating>Rating: </Rating><VoteAvg>${movie.vote_average.toFixed(1)}</VoteAvg>
@@ -52,11 +66,22 @@ const Movies: React.FC = ({category}) => {
                 ))}
         </GridMovies>
          <Pagination page={page} onNext={handleNext} onPrev={handlePrev} />
+         {showModal && selectedMovie && (
+                <MovieModal>
+                    <ModalContent>
+                        <CloseButton onClick={closeModal}>X</CloseButton>
+                        <h2>{selectedMovie.title}</h2>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`}
+                            alt={selectedMovie.title}
+                        />
+                        <p><strong>Rating:</strong> {selectedMovie.vote_average}</p>
+                        <p><strong>Overview:</strong> {selectedMovie.overview}</p>
+                        <p><strong>Release Date:</strong> {selectedMovie.release_date}</p>
+                    </ModalContent>
+                </MovieModal>
+            )}
         </>
-
-        
-
-       
     )
 }
 const GridMovies = styled.div`
@@ -94,4 +119,48 @@ const Rating = styled.h4`
 
 const VoteAvg = styled(Rating)``;
 
+const MovieModal = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0; 
+  left: 0;
+  width: 100%; 
+  height: 100%;
+  background: rgba(0,0,0,0.8);
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const ModalContent = styled.div`
+ background: #222;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 600px;
+  width: 90%;
+  color: #fff;
+  position: relative;
+
+  img {
+    height: 400px;
+    border-radius: 6px;
+  }
+
+  h2 {
+    margin: 15px 0 10px;
+  }
+  
+  p {
+    margin: 8px 0;
+  }
+
+`;
+
+const CloseButton = styled.button`
+  top: 10px;
+  right: 15px;
+  font-size: 22px;
+  cursor: pointer;
+  color: #fff;
+`;
 export default Movies;
