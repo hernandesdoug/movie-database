99bc7a9e9a53e92c3d326ac6c020affd15f0abfc
Douglas Hernandes <douglashernandes@icloud.com>
2025-12-11 16:15:02 -0300

    ajuste total pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import Header from "../src/components/Header.tsx";
 import Footer from "../src/components/Footer.tsx";
 import Search from "../src/components/Search.tsx";
 import MovieList from "./components/Movies.tsx";
-
+import { useState } from "react";
 
 
 function App() {
- 
+    const [modeSearch, setmodeSearch] = useState(false);
     return (           
             <>
-            <Header />
-            <Search />   
-            <MovieList />  
+            <Header/>
+            <Search setmodeSearch={setmodeSearch}/>   
+            {modeSearch === false && (<MovieList />)}
             <Footer />
         </>     
     )
